dc4aedbb08cfc28c65ba47ef1e8ffd089f01c0a6
Gol4m <armored322@gmail.com>
2022-10-06 18:11:32 +0300

    rqst fix

--- neo4j_country_db/requests.py
+++ neo4j_country_db/requests.py
@@ -9,16 +9,16 @@ class Request:
     def close(self):
         self.driver.close()
 
-    def findCapitalName(self, country_name):
+    def findCitiesWithAccessToWater(self):
         with self.driver.session() as session:
-            capitalName = session.execute_write(self._findCapital, country_name)
+            city = session.execute_write(self._accessToSeaOrOceanCities)
+            print(city)
+
+    def findCapitalName(self):
+        with self.driver.session() as session:
+            capitalName = session.execute_write(self._findCapital)
             print(capitalName)
             return capitalName
-
-    def findCitiesWithAccessToWater(self, country_name):
-        with self.driver.session() as session:
-            city = session.execute_write(self._accessToSeaOrOceanCities, country_name)
-            print(city)
 
     def findFullInformationAboutCity(self, city_name):
         with self.driver.session() as session:
@@ -26,20 +26,16 @@ class Request:
             print(city)
 
     @staticmethod
-    def _accessToSeaOrOceanCities(tx, country_name):
-        result = tx.run("MATCH (country:Country {name:$country_name}) "  # Ищем страну по названию
-                        "MATCH (country)-[has_city:has_city]->(city:City)"  # Ищем города в этой стране по связи has_city
-                        "MATCH (city {accessToSeaOrOcean:true}) "  # Проверяем доступ к морю у найденного города
-                        "RETURN city.name",  # Возвращаем название города
-                        country_name=country_name)
+    def _accessToSeaOrOceanCities(tx):
+        result = tx.run("MATCH (city:City {accessToSeaOrOcean:true}) "  # Проверяем доступ к морю у найденного города
+                        "RETURN city.name")  # Возвращаем название города
         return result.single()[0]
 
     @staticmethod
-    def _findCapital(tx, country_name):
-        result = tx.run("MATCH (country:Country {name:$country_name}) "
+    def _findCapital(tx):
+        result = tx.run("MATCH (country:Country) "
                         "MATCH (country)-[capital:capital]->(city:City)"  # Ищем город по связи capital
-                        "RETURN city.name",
-                        country_name=country_name)
+                        "RETURN city.name")
         return result.single()[0]
 
     @staticmethod
@@ -53,7 +49,7 @@ class Request:
 
 if __name__ == "__main__":
     rq = Request("bolt://localhost:7999", "neo4j", "admin")
-    rq.findCitiesWithAccessToWater("Канада")
-    capital = rq.findCapitalName("Канада")
+    rq.findCitiesWithAccessToWater()
+    capital = rq.findCapitalName()
     rq.findFullInformationAboutCity(capital)
     rq.close()
