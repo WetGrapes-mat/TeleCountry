a4141fbbf3600a0ed33010f4603619e066b247d9
anyavasilenok <anyavasilenok02@gmail.com>
2022-11-15 17:09:55 +0300

    add_cost_living_request

--- neo4j_country_db/cost_living.py
+++ neo4j_country_db/cost_living.py
@@ -4,10 +4,19 @@ from neo4j_country_db.requests import Request
 class CostLiving(Request):
     def __init__(self):
         super().__init__()
+        self.prices = []
+
+    def get_information(self):
+        request = Request()
+        self.prices = request.findAllPrices()
+        for price in self.prices:
+            print(price)
+        request.close()
 
 
 cost_living_db = CostLiving()
 
 if __name__ == "__main__":
-    pass
+    cost_living_db = CostLiving()
+    cost_living_db.get_information()
 
