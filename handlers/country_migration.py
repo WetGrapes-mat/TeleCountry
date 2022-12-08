c8dd1aa3ee2b0f662883190f63b0f337705618fb
WetGrapes-mat <WetGrapes-mat>
2022-12-09 00:20:36 +0300

    request for agent

--- handlers/country_migration.py
+++ handlers/country_migration.py
@@ -1,6 +1,9 @@
 from aiogram import types, Dispatcher
 from create_bot import bot
 from neo4j_country_db import country_migration_db
+from keybords import country_migration
+
+answer_user = dict()
 
 
 async def command(message: types.Message) -> None:
@@ -8,6 +11,26 @@ async def command(message: types.Message) -> None:
         await bot.send_message(message.from_user.id, text="В разработке")
     except:
         pass
+
+
+async def members_amount(callback: types.CallbackQuery, callback_data: dict):
+    await callback.message.edit_text(text='Хотите жить в городе где есть выход к морю/океану', reply_markup=country_migration)
+    answer_user['washes'] = callback_data['action']
+
+
+async def child_preschool(callback: types.CallbackQuery, callback_data: dict):
+    await callback.message.edit_text(text='Выбирете какой климот хотите', reply_markup=country_migration)
+    answer_user['climat'] = callback_data['action']
+
+
+async def child_school(callback: types.CallbackQuery, callback_data: dict):
+    await callback.message.edit_text(text='Насколько важна комуникация на английском ', reply_markup=country_migration)
+    answer_user['communication'] = callback_data['action']
+
+
+async def smoking(callback: types.CallbackQuery, callback_data: dict):
+    await callback.message.edit_text(text='Развитость общественого транспорта', reply_markup=country_migration)
+    answer_user['transport'] = callback_data['action']
 
 
 def register_handlers(dp: Dispatcher):
--- neo4j_country_db/country_migration.py
+++ neo4j_country_db/country_migration.py
@@ -5,8 +5,143 @@ class CountryMigration(Request):
     def __init__(self):
         super().__init__()
 
+    def findClimat(self):
+        with self.driver.session() as session:
+            city = session.execute_write(self._climat)
+            return city
+
+    def findOcean(self):
+        with self.driver.session() as session:
+            city = session.execute_write(self._ocean)
+            return city
+
+    def findisb(self, isb):
+        with self.driver.session() as session:
+            city = session.execute_write(self._isbig, isb)
+            return city
+
+    def findenglish(self):
+        with self.driver.session() as session:
+            city = session.execute_write(self._english)
+            return city
+
+    def findglobalrank(self):
+        with self.driver.session() as session:
+            city = session.execute_write(self._globalrank)
+            return city
+
+    def findtransport(self):
+        with self.driver.session() as session:
+            city = session.execute_write(self._transport)
+            return city
+
+    def findlgbt(self):
+        with self.driver.session() as session:
+            city = session.execute_write(self._lgbt)
+            return city
+
+    def findlifetype(self):
+        with self.driver.session() as session:
+            city = session.execute_write(self._life_type)
+            return city
+
+    @staticmethod
+    def _climat(tx):
+        result = tx.run("MATCH (n:Country) -[:climat]->(c:Climat) return n as Country ,"
+                        "n.name as name,"
+                        "c.averageDurationOfWinter as averageDurationOfWinter,"
+                        "c.juneAverageTemperature as juneAverageTemperature,"
+                        "c.decemberAverageTemperature as decemberAverageTemperature,"
+                        "c.airPollution as airPollution,"
+                        "c.waterPollution as waterPollution,"
+                        "c.dirtyAndUntidy as dirtyAndUntidy, "
+                        "c.comfortableToSpendTimeInTheCity as comfortableToSpendTimeInTheCity")
+        return [{'name':info["name"], 'averageDurationOfWinter': info['averageDurationOfWinter'],
+                 'juneAverageTemperature': info['juneAverageTemperature'],
+                 'decemberAverageTemperature': info['decemberAverageTemperature'],
+                 'airPollution': info['airPollution'],
+                 'waterPollution': info['waterPollution'],
+                 'dirtyAndUntidy': info['dirtyAndUntidy'],
+                 'comfortableToSpendTimeInTheCity': info['comfortableToSpendTimeInTheCity']} for info in result]
+
+    @staticmethod
+    def _ocean(tx):
+        result = tx.run("match (h:Country)-[]->(n:City)<-[:washes]-() return  "
+                        "h.name as nameCountry,"
+                        "n.name as nameCity")
+        return [{'nameCountry': info["nameCountry"], 'nameCity': info['nameCity']} for info in result]
+
+    @staticmethod
+    def _isbig(tx, isb):
+        result = tx.run("match (h:Country)-[:has_city]->(n:City {isBig: $isb}) return "
+                        "h.name as nameCountry,"
+                        "n.name as nameCity", isb=isb)
+        return [{'nameCountry': info["nameCountry"], 'nameCity': info['nameCity']} for info in result]
+
+
+    @staticmethod
+    def _english(tx):
+        result = tx.run("match (h:Country)-[:communication]->(n:Communication) return "
+                        "h.name as nameCountry,"
+                        "n.communicationOnEnglish as communicationOnEnglish")
+        return [{'nameCountry': info["nameCountry"], 'communicationOnEnglish': info['communicationOnEnglish']} for info in result]
+
+    @staticmethod
+    def _globalrank(tx):
+        #чем больше тем хуже
+        result = tx.run("match (h:Country)-[:citizenship]->(n:Citizenship) return "
+                        "h.name as nameCountry,"
+                        "n.globalRank as globalRank")
+        return [{'nameCountry': info["nameCountry"], 'globalRank': info['globalRank']} for info
+                in result]
+
+    @staticmethod
+    def _transport(tx):
+        result = tx.run("match (h:Country)-[:transport]->(n:Transport) return "
+                        "h.name as nameCountry,"
+                        "n.developmentLevelOfPublicTransport as developmentLevelOfPublicTransport")
+        return [{'nameCountry': info["nameCountry"], 'developmentLevelOfPublicTransport': info['developmentLevelOfPublicTransport']} for info
+                in result]
+
+    @staticmethod
+    def _lgbt(tx):
+        result = tx.run("match (h:Country)-[:security]->(n:Security) return "
+                        "h.name as nameCountry,"
+                        "n.attitudeTowardsLGBT as attitudeTowardsLGBT")
+        return [{'nameCountry': info["nameCountry"],
+                 'attitudeTowardsLGBT': info['attitudeTowardsLGBT']} for info
+                in result]
+
+    @staticmethod
+    def _life_type(tx):
+        result = tx.run("match (h:Country)-[:population]->(n:Population) return "
+                        "h.name as nameCountry,"
+                        "n.nightLifeEntertainment as nightLifeEntertainment,"
+                        "n.speedOfLife as speedOfLife,"
+                        "n.workPlaces as workPlaces")
+        return [{'nameCountry': info["nameCountry"],
+                 'nightLifeEntertainment': info['nightLifeEntertainment'],
+                 'speedOfLife': info['speedOfLife'],
+                 'workPlaces': info['workPlaces']} for info
+                in result]
+
+
+
+
+
 
 country_migration_db = CountryMigration()
 
 if __name__ == "__main__":
-    pass
+    # c = country_migration_db.findClimat()
+    # for i in c:
+    #     z = (i['airPollution'] + i['waterPollution'] + i['dirtyAndUntidy'])//3
+    #     x = i['comfortableToSpendTimeInTheCity']
+    #     if i['decemberAverageTemperature'] == 0: i['decemberAverageTemperature'] = -1
+    #     y = (((i['averageDurationOfWinter'] * i['decemberAverageTemperature'])/2) + i['juneAverageTemperature'])/2
+    #     print(i['name'], z, "грязь\n", x,"комфорт\n", y , "температура" )
+    print('+++++++++++++')
+    c = country_migration_db.findlifetype()
+    print(c)
+
+
