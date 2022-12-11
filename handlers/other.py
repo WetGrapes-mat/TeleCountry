485d3e2f9fe82112413ff356054e696c1fe19719
WetGrapes-mat <WetGrapes-mat>
2022-12-12 01:24:53 +0300

    send photo

--- handlers/other.py
+++ handlers/other.py
@@ -1,4 +1,6 @@
 from aiogram import types, Dispatcher
+from aiogram.types import InputMediaPhoto
+
 from create_bot import bot
 from keybords import other
 from neo4j_country_db import rq
@@ -15,6 +17,15 @@ async def command_start(message: types.Message):
         pass
 
 
+async def command_test(message: types.Message):
+
+    # await bot.send_message(message.from_user.id, '<a href="https://www.estudarfora.org.br/wp-content/uploads/2020/04/unipo.jpg">&#8203;</a>', parse_mode="HTML")
+    await bot.send_media_group(message.chat.id, [InputMediaPhoto('https://www.estudarfora.org.br/wp-content/uploads/2020/04/unipo.jpg','привет'),
+                                           InputMediaPhoto('https://www.ru.studies-in-europe.eu/img/uczelnie/a1673/g/Berlin-Universitat-zwischen-1890-und-1900-p2961.jpg'),
+                                           InputMediaPhoto('https://www.univ-tlse2.fr/medias/photo/universite_1500638826879-jpg?ID_FICHE=191397')]
+                               )
+
+
 async def echo(message: types.Message) -> None:
     try:
         await bot.send_message(message.from_user.id, text=message.text)
@@ -22,53 +33,12 @@ async def echo(message: types.Message) -> None:
         pass
 
 
-async def water_cb(callback: types.CallbackQuery, callback_data: dict) -> None:
-    await callback.message.edit_text(text='Столица', reply_markup=other.ikb_capital)
-    answer_user['accessToSeaOrOcean'] = callback_data['action']
-
-
-async def cities_are_good():
-    final_message = str()
-    cities_are_good = list()
-
-    for c in rq.findCitiesWithAccessToWater(eval(answer_user['accessToSeaOrOcean'])):
-        cities_are_good.append(c)
-
-    if eval(answer_user['capital']) is True:
-        cities = rq.findCapitalName(cities_are_good)
-        for city in cities:
-            info = rq.findFullInformationAboutCity(city)
-            final_message += f'Название города: {info[0]["city.name"]}\n' \
-                             f'Есть выход к морю: {info[0]["city.accessToSeaOrOcean"]}\n' \
-                             f'Столица: True\n ' \
-                             f'===================\n'
-    elif eval(answer_user['capital']) is False:
-        try:
-            cities = rq.findCapitalName(cities_are_good)
-            for _ in cities:
-                cities_are_good.remove(_)
-        except:
-            pass
-        for city in cities_are_good:
-            info = rq.findFullInformationAboutCity(city)
-            final_message += f'Название города: {info[0]["city.name"]}\n' \
-                             f'Есть выход к морю: {info[0]["city.accessToSeaOrOcean"]}\n' \
-                             f'===================\n'
-    return final_message
-
-
-async def capital_cb(callback: types.CallbackQuery, callback_data: dict) -> None:
-    answer_user['capital'] = callback_data['action']
-    final_message = await cities_are_good()
-    if final_message:
-        await callback.message.edit_text(text=final_message)
-    else:
-        await callback.message.edit_text(text="Нет городов которые вам походят")
-
 
 
 def register_handlers_other(dp: Dispatcher):
     dp.register_message_handler(command_start, commands=['start'])
+    dp.register_message_handler(command_test, commands=['test'])
+
     dp.register_message_handler(echo)
     # dp.register_callback_query_handler(water_cb, other.cb_water.filter())
     # dp.register_callback_query_handler(capital_cb, other.cb_capital.filter())
